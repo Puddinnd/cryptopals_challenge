2ce8dbaab783aaecda9956e1ac76dff07875c97a
CokeFung <naruebetcm@gmail.com>
2021-04-08 00:23:10 +0700

    resolved challenge 2

--- Set-1-Basics/2-fixed_xor.py
+++ Set-1-Basics/2-fixed_xor.py
@@ -1,9 +1,24 @@
-def xor(a, b):
-    c = ""
-    for i in range(len(a)):
-        c += chr(ord(a[i])^ord(b[i]))
-    return c.encode()
+import binascii
 
-a = bytes.fromhex("1c0111001f010100061a024b53535009181c").decode('utf-8')
-b = bytes.fromhex("686974207468652062756c6c277320657965").decode('utf-8')
-print( xor(a, b).hex() )
+a = "1c0111001f010100061a024b53535009181c"
+b = "686974207468652062756c6c277320657965"
+
+a = bytes.fromhex(a)
+b = bytes.fromhex(b)
+
+print(a)
+print(b)
+
+### First way to xor
+ans = ""
+for i in range(len(a)):
+	ans += chr(a[i]^b[i])
+print(ans)
+
+### Second way to xor
+ans = "".join(list(map(lambda x,y: chr(x^y), a, b)))
+print(ans)
+
+### Convert string to hex
+print(ans.encode().hex())
+print(binascii.hexlify(ans.encode()))
