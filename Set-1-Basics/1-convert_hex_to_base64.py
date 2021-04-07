99b330a26ebc012312f970b83bace138b670cab2
CokeFung <naruebetcm@gmail.com>
2021-04-07 23:53:08 +0700

    re-solved challenge 1

--- Set-1-Basics/1-convert_hex_to_base64.py
+++ Set-1-Basics/1-convert_hex_to_base64.py
@@ -1,5 +1,16 @@
 import base64
-
+import binascii
 hex_str = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
 
-print(base64.b64encode(bytes.fromhex(hex_str)).decode())
+### Hex to Ascii in 2 ways 
+print(binascii.unhexlify(hex_str))
+print(bytes.fromhex(hex_str))
+
+### Bytes to String
+text = bytes.fromhex(hex_str)
+text = text
+print(text)
+
+### Ascii to base64
+print(binascii.b2a_base64(text))
+print(base64.b64encode(text))
